0a522fb6a863399c91f19de8c1c4ce36718d9f6c
Aleksey <zawertuy@gmail.com>
2021-12-06 00:42:47 +1200

    8Add

--- myFirstSite/src/Controller/Main/HotSpringsController.php
+++ myFirstSite/src/Controller/Main/HotSpringsController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Main;
 
+use App\Entity\Comment;
 use App\Entity\Place;
 use App\Entity\Image;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +17,7 @@ class HotSpringsController extends BaseController
     public function indexHotSprings (): Response
     {
         $forRender = parent::renderDefault();
-        $forRender['title'] = 'Горячие источники';
+        $forRender['title'] = 'Горячие источники Камчатки';
         $forRender['hotSprings'] = $this->getDoctrine()->getRepository(Place::class)
                                         ->findBy(['category_place' => '2']);
         
@@ -23,7 +26,7 @@ class HotSpringsController extends BaseController
     }
 
     #[Route('/hot_springs/{id}', name: 'hot_springs_id')]
-    public function showOneVolcano (int $id): Response
+    public function showOneHotSpring (int $id, Request $request, CommentRepository $commentRepository, Place $place): Response
     {
         $place = $this->getDoctrine()->getRepository(Place::class)
                                      ->find($id);
@@ -33,10 +36,44 @@ class HotSpringsController extends BaseController
 
         $images = $this->getDoctrine()->getRepository(Image::class)
                                       ->findBy(['place' => $id]);
+       
         $forRender = parent::renderDefault();
         $forRender['place'] = $place;
         $forRender['images'] = $images;
+        
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($place, $offset);
+        $forRender['comments'] = $paginator;
+        $forRender['previous'] = $offset - CommentRepository::PAGINATOR_PER_PAGE;
+        $forRender['next'] = min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE);
+
+        // if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+        //     $this->addFlash(
+        //        'success',
+        //        'Только зарегистрированные пользователи могут оставлять комментарии.'
+        //     );
+        // } else {
+        $newComment = new Comment();
+        $newComment->setUser($this->getUser());
+        $place->addComment($newComment);
+        $newComment->setIsPublished();
+        $form = $this->createForm(CommentType::class, $newComment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newComment);
+            $em->flush();
+
+            return $this->redirectToRoute('hot_springs_id', ['id' => $id]);
+        }
+    
+        $forRender['form'] = $form->createView();
         return $this->render('main/place/hotSprings/oneHotSpring.html.twig', $forRender);
+
+        // }
+        
+        
     }
    
 }
--- myFirstSite/src/Form/CommentType.php
+++ myFirstSite/src/Form/CommentType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,23 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-            ->add('create_at')
-            ->add('is_published')
-            ->add('post')
-        ;
+            ->add('user_name', TextType::class, array (
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Введите имя',
+                ]))
+            ->add('content', TextareaType::class, array (
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Текст',
+                ]))
+            ->add('submit', SubmitType::class, array (
+                'label' => 'Разместить',
+                'attr' => [
+                    'class' => 'btn btn-success  mt-2',
+                ]
+            ));
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
